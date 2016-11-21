560e48c7c5ed4bf51eaa0026bfbf162bb8618617
Justin Seals <justin.seals@bplmarketing.com>
2016-11-21 16:33:32 +0000

    Mushroom2

---
+++ mushroom-bites.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+
+<head>
+
+	<title>Justin Seals Helpful Recipe Website - Blank Recipe</title>
+    
+	<?php include '_includes/header-inc.php'; ?> 
+
+</head>
+<body>
+
+<div id="pagewidth">
+	
+    <?php include '_includes/masthead.php'; ?> 
+      
+    <div id="main" class="inner-recipe">
+		<div id="hero">
+			<h1>Recipe</h1>
+			<img src="_images/food-heros/item-hero.jpg" alt="#" />
+		</div><!--Hero End-->
+			
+		<div id="ingredients">
+			<h2>Ingredients (10 bites):</h2>
+			<p><strong>Food:</strong></p>
+			<ul>
+				<li>10 small portebella mushrooms</li>
+				<li>5 slices of bacon</li>
+				<li>80g of cream cheese (Philadelphia)</li>
+				<li>50g of grated cheddar/mozeralla</li>
+			</ul>
+			<p><strong>Utensils:</strong></p>
+			<ul>
+				<li>Baking tray</li>
+			</ul>
+		</div><!--Ingredients End-->
+		
+		<div id="instructions">
+			<h2>Instructions</h2>
+			<p><strong>Prepare (15min):</strong></p>
+			<p class="notice">Can make up to 24h in advance, cover and keep in the fridge</p>
+			<ol>
+				<li>Cut bacon into small squares, fry until just turning crispy, remove from heat and drain of fat</li>
+				<li>Mix cream cheese, cheese in a bowl and then mix in bacon</li>
+				<li>Take out stems of mushrooms, put a spoonfull of filling on top of each mushroom, place on a baking tray and cover</li>
+			</ol>
+			<p><strong>Cooking (xmin):</strong></p>
+			<ol>
+				<li>Put in 175 degree oven for 25 minutes, take out and leave to rest for 5 mintues before serving</li>
+			</ol>
+		</div><!--Instructions End-->
+	
+		<?php include '_includes/back-button.php'; ?> 
+			
+    </div><!--Main End-->
+    
+    <?php include '_includes/footer.php'; ?> 
+
+</div><!--Pagewidth End-->
+
+</body>
+</html>
